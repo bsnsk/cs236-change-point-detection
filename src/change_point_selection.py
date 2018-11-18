8d4548c409999b6f46b0ba1aa61ab38854b23d6d
Shuyang Shi <shuyang790@gmail.com>
2018-11-18 15:45:19 -0800

    Adding: change point selection + evaluation

---
+++ src/change_point_selection.py
@@ -0,0 +1,116 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+import json
+from data_loader import load_eeg, sliding_window
+from autoencoder import AutoEncoder
+from evaluate import draw_roc_threshold
+import matplotlib  # a workaround for virtualenv on maxOS
+matplotlib.use('TkAgg')  # a workaround for virtualenv on maxOS
+import matplotlib.pyplot as plt
+
+# directory info
+# choose different experiment runs by changing exp_path
+base_path = "./"
+exp_path = "{}experiments/2018-11-18 22:11:30.930851/".format(base_path)
+args_path = "{}args.txt".format(exp_path)
+checkpoint_path = "{}checkpoints/best.pt".format(exp_path)
+
+# Load arguments for the model
+with open(args_path, "r") as f:
+    args_dict = f.read()
+args = json.loads(args_dict)
+
+device = 'cpu'  # predict on CPU
+
+# load data and obtain latent representations z
+X_raw, y_raw = load_eeg("{}data/raw/".format(base_path))
+print("y_raw: {}".format(y_raw.shape))
+X_sliding = sliding_window(X_raw, args["window_size"])
+X_variable = Variable(torch.Tensor(X_sliding), requires_grad=False).to(device)
+auto_encoder = AutoEncoder(input_dim=X_sliding.shape[1],
+                           hidden_sizes=args["hidden_sizes"],
+                           latent_dim=args["latent_dim"],
+                           ).to(device)
+auto_encoder.load_state_dict(torch.load(checkpoint_path, map_location=device))
+z = auto_encoder.encode(X_variable).detach().numpy()
+print(z.shape)
+
+
+def find_peaks(z):
+    dists = np.sqrt(np.sum(np.diff(z, axis=0) ** 2, axis=1))
+    print(dists.shape)
+
+    def mean(xs):
+        return sum(xs) * 1. / len(xs)
+
+    # inspect width, i.e. for t we inspect [t-d, t+d]
+    d = 80
+    indices = [
+        i
+        for i in range(d, dists.shape[0] - d)
+        if dists[i] > max(dists[i-d:i]) and dists[i] > max(dists[i+1:i+d+1])
+        and mean(dists[i-d:i]) < mean(dists[i-d//2:i+d//2])
+        and mean(dists[i+1:i+d]) < mean(dists[i-d//2:i+d//2])
+    ]
+
+    # remove candidates that are very close
+    result = [indices[0]]
+    for idx in indices:
+        if idx - result[-1] >= d * 2:
+            result.append(idx)
+    indices = result
+    print("# indices ({}): {}".format(len(indices), indices))
+    return indices, dists
+
+
+def visualize(X_raw, y_raw, indices):
+    featureLB = 3000
+    featureUB = 5000
+    plt.clf()
+
+    # feature 0
+    xs = [x
+          for x in range(X_raw.shape[0])
+          if X_raw[x, 0] < featureUB and X_raw[x, 0] > featureLB]
+    ys = [X_raw[x, 0] for x in xs]
+    plt.plot(xs, ys, '-b', label="feature 0")
+
+    # feature 1
+    xs = [x
+          for x in range(X_raw.shape[0])
+          if X_raw[x, 1] < featureUB and X_raw[x, 1] > featureLB]
+    ys = [X_raw[x, 1] for x in xs]
+    plt.plot(xs, ys, '-g', label="feature 1")
+
+    # detected indices
+    alarmLB = 3500
+    alarmUB = 4500
+    plt.plot([], [], '-c', label="detected")
+    for idx in indices:
+        plt.plot([idx - 1, idx - 1], [alarmLB, alarmUB], '-c', linewidth=0.7)
+
+    # label
+    labelLB = 3900
+    labelUB = 4800
+    plt.plot([], [], '-r', label="label")
+    truths = [x for x in range(1, y_raw.shape[0]) if y_raw[x] != y_raw[x - 1]]
+    for x in truths:
+        plt.plot([x, x], [labelLB, labelUB], '-r', linewidth=0.7)
+    print("# labels ({}): {}".format(len(truths), truths))
+    print("# overlap {}".format(len([1 for t in indices if t in truths])))
+
+    plt.legend()
+    plt.xlabel("time")
+    plt.ylabel("amplitude")
+    plt.savefig("img/visualization.eps")
+    plt.savefig("img/visualization.png")
+
+
+indices, dists = find_peaks(z)
+visualize(X_raw, y_raw, indices)
+
+print("# dists: {}".format([dists[i] for i in indices]))
+
+truths = [x for x in range(1, y_raw.shape[0]) if y_raw[x] != y_raw[x - 1]]
+draw_roc_threshold(indices, truths, dists)
---
+++ src/evaluate.py
@@ -0,0 +1,59 @@
+import matplotlib  # a workaround for virtualenv on maxOS
+matplotlib.use('TkAgg')  # a workaround for virtualenv on maxOS
+import matplotlib.pyplot as plt
+
+
+# WARNING: Deprecated
+def draw_roc(y_hats, ys, length):
+    taus = range(0, 5000, 2)
+    points = []
+    for tau in taus:
+        num_correct = 0
+        num_alarm, cur_y_hats = len(y_hats), y_hats
+        num_truth, cur_ys = len(ys), ys
+        for y_hat in cur_y_hats:
+            y_closest = min((abs(y - y_hat), y) for y in cur_ys)[1]
+            y_back = min((abs(y - y_closest), y) for y in cur_y_hats)[1]
+            if y_back == y_hat and abs(y_hat - y_closest) <= tau:
+                num_correct += 1
+        TPR = num_correct * 1. / num_truth
+        FPR = 1 - num_correct * 1. / num_alarm
+        print("tau={}, num_correct={}, len_y={}, len_y_hat={}, TPR={}, FPR={}".format(
+            tau, num_correct, len(ys), len(y_hats), TPR, FPR))
+        points.append((FPR, TPR))
+    points = sorted(points)
+    plt.clf()
+    plt.plot([p[0] for p in points], [p[1] for p in points], '.-')
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.savefig("./img/roc.png")
+
+
+# draw ROC curve by varying distance threshold tau
+def draw_roc_threshold(y_hats, ys, dists):
+    taus = sorted([dists[i] for i in y_hats]) + [1.0]
+    tolerance = 200
+    points = []
+    for tau in taus:
+        num_correct = 0
+        cur_y_hats = [y for y in y_hats if dists[y] >= tau]
+        num_truth = len(ys)
+        num_alarm = len(cur_y_hats)
+        if num_alarm == 0:
+            continue
+        for y_hat in cur_y_hats:
+            y_closest = min((abs(y - y_hat), y) for y in ys)[1]
+            y_back = min((abs(y - y_closest), y) for y in cur_y_hats)[1]
+            if y_back == y_hat and abs(y_hat - y_closest) <= tolerance:
+                num_correct += 1
+        TPR = num_correct * 1. / num_truth
+        FPR = 1 - num_correct * 1. / num_alarm
+        print("tau={}, num_correct={}, len_y={}, len_y_hat={}, cur_len_y_hat={}, TPR={}, FPR={}".format(
+            tau, num_correct, len(ys), len(y_hats), num_alarm, TPR, FPR))
+        points.append((FPR, TPR))
+    points = sorted(points)
+    plt.clf()
+    plt.plot([p[0] for p in points], [p[1] for p in points], '.-')
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.savefig("./img/roc.png")

86473d721e507904c0ab3f5848e7fdece7816eb0
Shuyang Shi <shuyang790@gmail.com>
2018-11-29 00:39:09 +0000

    Scaling latent loss to match prediction loss in NICE

--- scripts/run_nice.sh
+++ scripts/run_nice.sh
@@ -3,7 +3,7 @@ python src/run_flow.py   --data_dir "data/raw/" \
                                 --iter_max 20000 \
                                 --iter_eval 50 \
                                 --batch_size 500 \
-                                --learning_rate 1e-1 \
-                                --reg 1e-3 \
+                                --learning_rate 1e-2 \
+                                --reg 0 \
                                 --window_size 100 \
-                                --hidden_sizes 300 300 300 300 300
+                                --hidden_sizes 100 100 100 100

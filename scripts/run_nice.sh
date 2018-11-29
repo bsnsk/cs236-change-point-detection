110f0496f2013fcb1c3d40b177e39042d9b488eb
Shuyang Shi <shuyang790@gmail.com>
2018-11-29 04:42:47 +0000

    add regularization to flow script

--- scripts/run_nice.sh
+++ scripts/run_nice.sh
@@ -4,6 +4,6 @@ python src/run_flow.py   --data_dir "data/raw/" \
                                 --iter_eval 50 \
                                 --batch_size 500 \
                                 --learning_rate 1e-2 \
-                                --reg 0 \
+                                --reg 1e-3 \
                                 --window_size 100 \
                                 --hidden_sizes 100 100 100 100

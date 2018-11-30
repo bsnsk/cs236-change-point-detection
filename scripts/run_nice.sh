de635fa230308ba145abb5a11a8abeab723ec0ea
Shuyang Shi <shuyang790@gmail.com>
2018-11-29 19:03:14 -0800

    Adding dataset: HAR human activities

--- scripts/run_nice.sh
+++ scripts/run_nice.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-python src/run_flow.py          --data="eeg" \
+python src/run_flow.py          --data="har" \
                                 --data_dir "data/raw/" \
                                 --iter_max 20000 \
                                 --iter_eval 50 \
                                 --batch_size 500 \
                                 --learning_rate 1e-2 \
                                 --reg 1e-3 \
-                                --window_size 30 \
+                                --window_size 20 \
                                 --hidden_sizes 500 500 500 500
+
+# python src/run_flow.py          --data="eeg" \
+#                                 --data_dir "data/raw/" \
+#                                 --iter_max 20000 \
+#                                 --iter_eval 50 \
+#                                 --batch_size 500 \
+#                                 --learning_rate 1e-2 \
+#                                 --reg 1e-3 \
+#                                 --window_size 30 \
+#                                 --hidden_sizes 500 500 500 500
 
 # python src/run_flow.py   --data_dir "data/raw/" \
 #                                 --iter_max 20000 \

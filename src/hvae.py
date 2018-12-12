7f359b65d0a6ac1e1f911ce9429326565a576c70
Andy <andyyswoo@gmail.com>
2018-12-11 23:10:27 -0800

    shitty code of hvae

---
+++ src/hvae.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+class HVAE(nn.Module):
+
+    def __init__(self, generative, approx_post):
+        super(HVAE, self).__init__()
+        self.decoder = generative
+        self.encoder = approx_post
+        self.loss_func = nn.BCEWithLogitsLoss(reduction='sum')
+        self.ce = nn.BCELoss()
+
+    def forward(self, x):
+        batch = x.size(0)
+        dist, enc_samples = self.encoder(x)
+        sample_size = enc_samples.size(1)
+        # merge samples into batch for the decoder network
+        reshaped_samples = enc_samples.view(batch * sample_size,
+                                            *enc_samples.size()[2:])
+        output = self.decoder(reshaped_samples)
+        # reconstitute samples within a batch
+        output = output.view(batch, sample_size, *output.size()[1:])
+        return dist, output, reshaped_samples
+
+    def loss(self, X, y, dist, output, samples):
+        batch_size = X.size(0)
+        kl_loss = torch.sum(self.encoder.kl_loss(dist)) / batch_size
+
+        # data has dimension batch x pixels
+        # output has dimension batch x samples x pixels
+        sample_size = output.size(1)
+        truth = X.unsqueeze(1).expand(-1, sample_size, -1)
+        # approximate E_q[p(x|z)]
+        reconst_loss = self.loss_func(output, truth) / sample_size / batch_size
+
+        z_before = samples[[i for i in range(samples.size()[0]) if i % 2 == 0], :]
+        z_after = samples[[i for i in range(samples.size()[0]) if i % 2 == 1], :]
+        probs = torch.sigmoid(torch.norm(z_before - z_after, 2, 1, keepdim=True))
+        pred_loss = self.ce(probs, y) / batch_size
+
+        try:
+            y_np, prob_np = y.cpu().detach().numpy(), probs.cpu().detach().numpy()
+            auc = roc_auc_score(y_np, prob_np)
+            auprc = average_precision_score(y_np, prob_np)
+        except:
+            auc, auprc = 0, 0
+
+        total_loss = kl_loss + reconst_loss + pred_loss
+
+        return total_loss, reconst_loss, kl_loss, pred_loss, auc, auprc
+
+
+class GenerativeModel(nn.Module):
+    """The generative model (decoder) for the VAE.
+
+    The decoder takes as input multiple layers of variables with Gaussian
+    prior. It outputs some encoded state vector.
+
+    The layer transform[i] must take a tensor of size [batch x state_dim[i]] to
+    one of size [batch x state_dim[i+1]], or [batch_size x (output_shape)] for
+    the last transform.
+    """
+    def __init__(self, state_dims, transforms):
+        super().__init__()
+        if len(state_dims) == 0:
+            raise ValueError('Must have a nonzero number of generative parameters')
+        if len(state_dims) != len(transforms):
+            raise ValueError('Cannot have different numbers of generative parameters and transforms')
+        for idx, t in enumerate(transforms):
+            self.add_module('layer_{}'.format(idx), t)
+        self.layers = transforms
+        self.state_dims = state_dims
+        indices = [0]
+        for dim in state_dims:
+            indices.append(indices[-1] + dim)
+        self.indices = indices
+
+    def forward(self, x):
+        """Apply the decoder to encoding x.
+        Input should be a list, where entry i has shape [batch x state_dim[i]]
+        """
+        dev = x.device
+        # additive identity for ease of coding
+        result = torch.zeros(self.state_dims[0], device=dev)
+        for index, layer in enumerate(self.layers):
+            inp = x[:, self.indices[index]:self.indices[index+1]]
+            result = layer(result + inp)
+        return result
+
+    def sample(self, batch_size):
+        """Generate a sample from this distribution's prior."""
+        # TODO: make this work on GPU as well
+        dim = self.indices[-1]
+        enc = torch.randn(batch_size, dim)
+        params = self.forward(enc)
+        return torch.sigmoid(params)
+
+
+def build_mlp(input_dim, mlp_sizes):
+    layers = []
+    current_dim = input_dim
+    for size in mlp_sizes[:-1]:
+        layers.append(nn.Linear(current_dim, size))
+        layers.append(nn.ReLU())
+        current_dim = size
+    layers.append(nn.Linear(current_dim, mlp_sizes[-1]))
+    return nn.Sequential(*layers)
+
+
+def mlp_generative_model(state_dims, mlp_hidden_sizes, output_shape):
+    """Factory function for GenerativeModels with MLP transformations"""
+    transforms = []
+    for i in range(len(state_dims) - 1):
+        dims = mlp_hidden_sizes[i] + [state_dims[i+1]]
+        transforms.append(build_mlp(state_dims[i], dims))
+    dims = mlp_hidden_sizes[-1] + [output_shape]
+    transforms.append(build_mlp(state_dims[-1], dims))
+    return GenerativeModel(state_dims, transforms)
+
+
+def kl_stdnorm_diag(mu, c_diag):
+    """Compute the KL-divergence from the standard normal to a normal with
+    mean mu and covariance c_diag.
+    mu - tensor of size [batch x dim]
+    c_diag - tensor of size [batch x dim]
+    """
+    return 0.5 * (-c_diag.log().sum(dim=1) - mu.size(1) + c_diag.sum(dim=1)
+                  + (mu * mu).sum(dim=1))
+
+
+class ApproxPosterior(nn.Module):
+    """Approximate posterior distribution (encoder) for the VAE.
+
+    Learns the parameters of one Gaussian per layer of the generative model.
+    Assumes that the distribution factors across layers.
+
+    The network is structured as an MLP, where the last layer has a number of
+    different outputs corresponding to the parameters of the decoder.
+    """
+
+    def __init__(self, input_dim, mlp_sizes, state_dims, sample_size,
+                 dropout=0.2):
+        super().__init__()
+        # TODO: allow length 0 mlp_sizes in a reasonable way
+        self.dropout = nn.Dropout(dropout)
+        self.representation = build_mlp(input_dim, mlp_sizes)
+        rep_dim = mlp_sizes[-1] if len(mlp_sizes) > 0 else input_dim
+        self.state_size = sum(state_dims)
+        self.output_mu = nn.Linear(rep_dim, self.state_size)
+        self.output_cov = nn.Linear(rep_dim, self.state_size)
+        self.sample_size = sample_size
+
+    def encode_distribution(self, x):
+        rep = F.relu(self.representation(self.dropout(x)))
+        mu = self.output_mu(rep)
+        cov = F.softplus(self.output_cov(rep))
+        return (mu, cov)
+
+    def forward(self, x):
+        """Sample from the approximate posterior.
+        Output is a list of tensors, where the tensor at index i has shape:
+        [batch x samples x state_dims[i]
+        """
+        # Use the fact that N(m, C) = sqrt(C) * N(0, 1) + m to allow autograd
+        # to backprop through the Gaussian parameters properly
+        batch = x.size(0)
+        dev = x.device
+        mu, cov = self.encode_distribution(x)
+        sample = torch.randn(batch, self.sample_size, self.state_size,
+                             device=dev)
+        sample = sample * torch.sqrt(cov.unsqueeze(1)) + mu.unsqueeze(1)
+        return (mu, cov), sample
+
+    def kl_loss(self, dist):
+        mu, cov = dist
+        return kl_stdnorm_diag(mu, cov)
---
+++ src/run_hvae.py
@@ -0,0 +1,142 @@
+import os, json, argparse, math
+from datetime import datetime
+import numpy as np
+import torch
+from tqdm import tqdm
+import tensorflow as tf
+
+from data_loader import load_and_build_tensors
+import hvae
+from train import write_summaries
+
+REPO_DIR = os.path.relpath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+# Data
+parser.add_argument("--data", choices=["eeg", "syn"], default="syn", help="Which dataset to use")
+parser.add_argument("--data_dir", type=str, help="Data directory")
+
+# Experiments
+parser.add_argument("--iter_max", type=int, default=20000, help="Max training iterations")
+parser.add_argument("--iter_eval", type=int, default=50, help="Evaluate dev set performance every n iterations")
+
+# Training
+parser.add_argument("--batch_size", type=int, default=500)
+parser.add_argument("--learning_rate", type=float, default=1e-1)
+parser.add_argument("--reg", type=float, default=0)
+
+# Model
+parser.add_argument("--window_size", type=int)
+parser.add_argument("--hidden_sizes", type=int, nargs="+")
+parser.add_argument("--latent_dim", type=int, nargs="+")
+
+args = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+input_dim, tensors = load_and_build_tensors(args.data, args, device)
+X_train, y_train = tensors['X_train'], tensors['y_train']
+X_dev, y_dev = tensors['X_dev'], tensors['y_dev']
+
+# TODO args in
+state_sizes = [100, 100]
+generative = hvae.GenerativeModel(state_sizes, [hvae.build_mlp(100, [100]), hvae.build_mlp(100, [input_dim])])
+recog = hvae.ApproxPosterior(input_dim, [100, 100], state_sizes, 1)
+model = hvae.HVAE(generative, recog).to(device)
+print('Model structure:')
+print(list(model.children()))
+
+exp_folder = os.path.join(REPO_DIR, "experiments/HVAE-"+args.data+"-"+str(datetime.now()))
+os.mkdir(exp_folder)
+with open(os.path.join(exp_folder, "args.txt"), "w") as fp:
+    fp.write(json.dumps(vars(args)))
+
+save_dir = os.path.join(exp_folder, "checkpoints")
+os.mkdir(save_dir)
+summary_writer = tf.summary.FileWriter(exp_folder)
+
+autoencoder=model
+epochs = args.iter_max
+batch_size=args.batch_size
+lr=args.learning_rate
+
+
+def train_batch(autoencoder, X, y, optimizer):
+    optimizer.zero_grad()
+    dist, output, samples = autoencoder(X)
+    loss, rec, kl, pred, auc, auprc = autoencoder.loss(X, y, dist, output, samples)
+    loss.backward()
+    optimizer.step()
+    return loss.item(), rec, kl, pred, auc, auprc
+
+
+def evaluate_elbo(autoencoder, X, y, device):
+    autoencoder.eval()
+    autoencoder = autoencoder.to(device=device)
+    X = X.to(device=device)
+    X = X.view(-1, X.shape[1] // 2)
+    y = y.to(device=device)
+    dist, output, samples = autoencoder(X)
+    loss, rec, kl, pred, auc, auprc = autoencoder.loss(X, y, dist, output, samples)
+    return loss, rec, kl, pred, auc, auprc
+
+n = X_train.size(0)
+
+# this will probably leave out the last couple training points...
+autoencoder = autoencoder.to(device=device)
+sgd = torch.optim.Adam(autoencoder.parameters(), lr)
+
+iter = 0
+best_dev_auc = 0
+train_size = X_train.shape[0]
+indices = np.arange(train_size)
+
+
+with tqdm(total=args.iter_max) as pbar:
+    while iter <= args.iter_max:
+        np.random.shuffle(indices)
+        for start in np.arange(0, train_size, args.batch_size):
+            autoencoder.train()
+            data = X_train[start:start+batch_size].to(device=device)
+            y_batch = y_train[start:start+batch_size].to(device=device)
+            data = data.view(-1, data.shape[1] // 2)
+            loss, rec, kl, pred, auc, auprc = train_batch(autoencoder, data, y_batch, sgd)
+
+            write_summaries(summary_writer, iter, "train", {
+                "total_loss": loss,
+                "nelbo": rec+kl, "rec_loss": rec, "kld": kl,
+                "ce_loss": pred,
+                "auc": auc, "auprc": auprc
+            })
+
+            if iter % args.iter_eval == 0:
+                autoencoder.eval()
+                with torch.no_grad():
+                    dev_loss, dev_rec, dev_kl, dev_pred, dev_auc, dev_auprc = evaluate_elbo(autoencoder, X_dev, y_dev, device)
+                    write_summaries(summary_writer, iter, "dev", {
+                        "total_loss": dev_loss,
+                        "nelbo": dev_rec+dev_kl, "rec_loss": dev_rec, "kld": dev_kl,
+                        "ce_loss": dev_pred,
+                        "auc": dev_auc, "auprc": dev_auprc
+                    })
+                if dev_auc > best_dev_auc:
+                    best_dev_auc = dev_auc
+                    file_path = os.path.join(save_dir, "best.pt")
+                    state = autoencoder.state_dict()
+                    torch.save(state, file_path)
+                    print("Best model saved to {}".format(file_path))
+                    summary = "Best dev auc: {}".format(dev_auc)
+                    with open(os.path.join(exp_folder, "best_stats.txt"), 'w') as fp:
+                        fp.write(summary)
+                    print(summary)
+            pbar.set_postfix(
+                train_loss="{:.2e}".format(loss),
+                train_auc="%.4f" % auc,
+                dev_loss="{:.2e}".format(dev_loss),
+                dev_auc="%.4f" % dev_auc
+            )
+            iter += 1
+            pbar.update(1)
+
+
